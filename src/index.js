a2ea3c70ff773ce4d0973d20293c1522f0230985
Nuno Silva <nunosilva800@gmail.com>
2018-10-26 09:11:37 -0300

    add basic html structure and detect web3

--- src/index.js
+++ src/index.js
@@ -1,13 +1,52 @@
 import './style.css';
 
-function component() {
+function errorMessageComponent(message) {
   let element = document.createElement('div');
-
-  // Lodash, currently included via a script, is required for this line to work
-  element.innerHTML = "Hello world";
-  element.classList.add('hello');
-
+  element.innerHTML = message;
+  element.classList.add('error-msg');
   return element;
 }
 
-document.body.appendChild(component());
+function noticeMessageComponent(message) {
+  let element = document.createElement('div');
+  element.innerHTML = message;
+  element.classList.add('notice-msg');
+  return element;
+}
+
+window.addEventListener('load', async () => {
+  // Modern dapp browsers...
+  if (window.ethereum) {
+    window.web3 = new Web3(ethereum);
+    try {
+      // Request account access if needed
+      await ethereum.enable();
+      document.body.appendChild(
+        noticeMessageComponent(`Connected to network ${web3.version.network}`)
+      );
+      document.body.appendChild(
+        noticeMessageComponent(
+          `Detected accounts: ${web3.eth.accounts.join(', ')}`
+        )
+      );
+      // Acccounts now exposed
+      //web3.eth.sendTransaction({/* ... */});
+    } catch (error) {
+      // User denied account access...
+      console.log(error);
+      document.body.appendChild(errorMessageComponent(error));
+    }
+  }
+  // Legacy dapp browsers...
+  else if (window.web3) {
+    window.web3 = new Web3(web3.currentProvider);
+    // Acccounts always exposed
+    //web3.eth.sendTransaction({/* ... */});
+  }
+  // Non-dapp browsers...
+  else {
+    document.body.appendChild(errorMessageComponent(
+      'Non-Ethereum browser detected. You should consider trying MetaMask!'
+    ));
+  }
+});
