587b9d76be67108023a5e2fbf028eaa08c1e6e3b
Nuno Silva <nunosilva800@gmail.com>
2018-10-26 09:23:05 -0300

    Refactor web3 init phase

--- src/index.js
+++ src/index.js
@@ -14,35 +14,41 @@ function noticeMessageComponent(message) {
   return element;
 }
 
-window.addEventListener('load', async () => {
+function initWeb3Ethereum() {
+  window.web3 = new Web3(ethereum);
+  try {
+    // Request account access if needed
+    ethereum.enable().then(web3Ready);
+  } catch (error) {
+    // User denied account access...
+    console.log(error);
+    document.body.appendChild(errorMessageComponent(error));
+  }
+}
+
+function initWeb3Legacy() {
+  window.web3 = new Web3(web3.currentProvider);
+  web3Ready();
+}
+
+function web3Ready() {
+  document.body.appendChild(
+    noticeMessageComponent(`Connected to network ${web3.version.network}`)
+  );
+  document.body.appendChild(
+    noticeMessageComponent(
+      `Detected accounts: ${web3.eth.accounts.join(', ')}`
+    )
+  );
+  // Acccounts now exposed
+  //web3.eth.sendTransaction({/* ... */});
+}
+
+window.addEventListener('load', () => {
   // Modern dapp browsers...
-  if (window.ethereum) {
-    window.web3 = new Web3(ethereum);
-    try {
-      // Request account access if needed
-      await ethereum.enable();
-      document.body.appendChild(
-        noticeMessageComponent(`Connected to network ${web3.version.network}`)
-      );
-      document.body.appendChild(
-        noticeMessageComponent(
-          `Detected accounts: ${web3.eth.accounts.join(', ')}`
-        )
-      );
-      // Acccounts now exposed
-      //web3.eth.sendTransaction({/* ... */});
-    } catch (error) {
-      // User denied account access...
-      console.log(error);
-      document.body.appendChild(errorMessageComponent(error));
-    }
-  }
+  if (window.ethereum) { initWeb3Ethereum() }
   // Legacy dapp browsers...
-  else if (window.web3) {
-    window.web3 = new Web3(web3.currentProvider);
-    // Acccounts always exposed
-    //web3.eth.sendTransaction({/* ... */});
-  }
+  else if (window.web3) { initWeb3Legacy() }
   // Non-dapp browsers...
   else {
     document.body.appendChild(errorMessageComponent(
