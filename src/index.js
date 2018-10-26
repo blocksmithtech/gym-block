4ae667aa348ae27752dc645b8fad5de2ade0a12d
Nuno Silva <nunosilva800@gmail.com>
2018-10-26 08:07:47 -0300

    Add style loader

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
+import './style.css';
+
 function component() {
   let element = document.createElement('div');
 
   // Lodash, currently included via a script, is required for this line to work
   element.innerHTML = "Hello world";
+  element.classList.add('hello');
 
   return element;
 }
