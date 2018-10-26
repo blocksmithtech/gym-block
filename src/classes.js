cf0217609e0b09b49858897e3f4fc6bdd04e8514
Nuno Silva <nunosilva800@gmail.com>
2018-10-26 14:58:47 -0300

    Add our smart contract

--- src/classes.js
+++ src/classes.js
@@ -1,39 +1,4 @@
-function deployContract(name, noShowAddress) {
-  // load contract from ABI
-  const contractABI = [];
-  var contract = web3.eth.contract(contractABI);
-
-  console.log('Deploying contract named: ', name);
-  // deploy new contract
-  contract.new(
-    name, noShowAddress,
-    {
-      from: web3.eth.coinbase,
-      data: '0x12345...',
-      gas: 1000000
-    },
-    (err, contractInstance) => {
-      if (!err) {
-        // NOTE: The callback will fire twice!
-        // Once the contract has the transactionHash property set and
-        // once its deployed on an address.
-        if (!contractInstance.address) {
-          // The hash of the transaction, which deploys the contract
-          console.log(
-            'Contract is being deployed in transaction: ',
-            contractInstance.transactionHash
-          );
-        } else {
-          // check address on the second call (contract deployed)
-          console.log('Contract deployed at: ', contractInstance.address);
-          window.location.href = `/attend?contract=${contractInstance.address}`;
-        }
-      } else {
-        console.log(err);
-      }
-    }
-  );
-}
+import deployContract from './contract_deploy.js'
 
 window.addEventListener('load', () => {
   let formElem = document.getElementById('new_class_form');
@@ -45,8 +10,19 @@ window.addEventListener('load', () => {
       let noShowAddressInputElem = formElem.querySelector(
         'input[name="noShowAddress"]'
       );
+      let depositInputElem = formElem.querySelector(
+        'input[name="deposit"]'
+      );
+      let limitOfParticipantsInputElem = formElem.querySelector(
+        'input[name="limitOfParticipants"]'
+      );
 
-      deployContract(nameInputElem.value, noShowAddressInputElem.value);
+      deployContract(
+        noShowAddressInputElem.value,
+        nameInputElem.value,
+        depositInputElem.value,
+        limitOfParticipantsInputElem.value
+      );
     });
   }
 });
